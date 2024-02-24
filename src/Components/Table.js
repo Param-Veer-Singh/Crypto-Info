658b51a088abc15825541023cf0c1185605e5d43
Param-Veer-Singh <paramveersinghchampawat@gmail.com>
2024-02-24 20:28:15 +0530

    first commit

---
+++ src/Components/Table.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Table = ({ crypto }) => {
+  return (
+    <tr>
+      <td>
+        <img src={crypto.image} alt={crypto.name} style={{ width: '30px', height: '30px' }} />
+      </td>
+      <td>{crypto.name}</td>
+      <td className='symbol'>{crypto.symbol}</td>
+      <td>${crypto.current_price}</td>
+      <td>Mkt Cap : ${crypto.total_volume}</td>
+    </tr>
+  );
+};
+
+export default Table;
